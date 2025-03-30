b801bc46a0b7d1dacc73e084211618cc28aa4959
masayasviel <33654883+masayasviel@users.noreply.github.com>
2025-03-30 15:03:46 +0900

    :sparkles: 日程登録

--- mocks/src/apis/attendance.api.ts
+++ mocks/src/apis/attendance.api.ts
@@ -3,9 +3,28 @@ import { Hono } from 'hono';
 import { store } from '../store';
 import { Response400Error, Response404Error } from './errors';
 import { response200, response200noContent } from './response';
+import { RegisterAttendanceRequestValidate } from './serializers/build/register-attendance.interface';
 import { RegisterParticipantRequestValidate } from './serializers/build/register-participant.interface';
 
 const router = new Hono();
+
+router.post(
+  '',
+  typiaValidator('json', RegisterAttendanceRequestValidate, (result) => {
+    if (!result.success) {
+      throw new Response400Error();
+    }
+  }),
+  (c) => {
+    const res = c.req.valid('json');
+    const uuid = crypto.randomUUID();
+    store[uuid] = {
+      ...res,
+      schedules: [],
+    };
+    return response200noContent(c);
+  },
+);
 
 router.get('/:uuid', (c) => {
   const uuid = c.req.param('uuid');

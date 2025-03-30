7b889144023fd67b8090cde4340aac35f36b4d94
masayasviel <33654883+masayasviel@users.noreply.github.com>
2025-03-30 13:13:18 +0900

    :sparkles: 参加者API

--- mocks/src/apis/response.ts
+++ mocks/src/apis/response.ts
@@ -1,5 +1,9 @@
 import { Context } from 'hono';
 
+export function response200noContent(ctx: Context) {
+  return ctx.text('ok', 200);
+}
+
 export function response500(ctx: Context) {
   return ctx.text('Internal Server Error', 500);
 }

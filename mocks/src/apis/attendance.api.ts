cadcd6b61dc6aaa20b87cd89a21268d356123a12
masayasviel <33654883+masayasviel@users.noreply.github.com>
2025-03-30 13:34:02 +0900

    :truck:

--- mocks/src/apis/attendance.api.ts
+++ mocks/src/apis/attendance.api.ts
@@ -1,7 +1,9 @@
+import { typiaValidator } from '@hono/typia-validator';
 import { Hono } from 'hono';
 import { store } from '../store';
-import { Response404Error } from './errors';
-import { response200 } from './response';
+import { Response400Error, Response404Error } from './errors';
+import { response200, response200noContent } from './response';
+import { RegisterParticipantRequestValidate } from './serializers/build/register-participant.interface';
 
 const router = new Hono();
 
@@ -12,5 +14,26 @@ router.get('/:uuid', (c) => {
   }
   return response200(c, store[uuid]);
 });
+
+router.post(
+  '/:uuid',
+  typiaValidator('json', RegisterParticipantRequestValidate, (result) => {
+    if (!result.success) {
+      throw new Response400Error();
+    }
+  }),
+  (c) => {
+    const uuid = c.req.param('uuid');
+    if (!(uuid in store)) {
+      throw new Response404Error();
+    }
+    const res = c.req.valid('json');
+    store[uuid].schedules.push({
+      id: crypto.randomUUID(),
+      ...res,
+    });
+    return response200noContent(c);
+  },
+);
 
 export default router;
--- mocks/src/apis/participant.api.ts
+++ mocks/src/apis/participant.api.ts
@@ -3,31 +3,9 @@ import { Hono } from 'hono';
 import { store } from '../store';
 import { Response400Error, Response404Error } from './errors';
 import { response200noContent } from './response';
-import { RegisterParticipantRequestValidate } from './serializers/build/register-participant.interface';
 import { UpdateParticipantRequestValidate } from './serializers/build/update-participant.interface';
 
 const router = new Hono();
-
-router.post(
-  '/:uuid',
-  typiaValidator('json', RegisterParticipantRequestValidate, (result) => {
-    if (!result.success) {
-      throw new Response400Error();
-    }
-  }),
-  (c) => {
-    const uuid = c.req.param('uuid');
-    if (!(uuid in store)) {
-      throw new Response404Error();
-    }
-    const res = c.req.valid('json');
-    store[uuid].schedules.push({
-      id: crypto.randomUUID(),
-      ...res,
-    });
-    return response200noContent(c);
-  },
-);
 
 router.patch(
   '/:uuid/:participant',

0030c413883d11a95c163774f98b142464dd4f14
yasi1989 <kei.kobayashi.works1989@gmail.com>
2025-04-08 16:20:08 +0900

    fix:データ送受信をuseSWRに

--- src/app/attendance/[uuid]/register/RegisterContainer.tsx
+++ src/app/attendance/[uuid]/register/RegisterContainer.tsx
@@ -1,19 +1,22 @@
-import { fetchCandidateDates } from '@/lib/fetcher';
+'use client';
+import { use } from 'react';
 import { redirect } from 'next/navigation';
 import RegisterPresentational from './RegisterPresentational';
+import { useAttendance } from '@/lib/hooks/useAttendance';
 
 type RegisterContainerProps = {
   params: Promise<{ uuid: string }>;
 };
 
-const RegisterContainer = async ({ params }: RegisterContainerProps) => {
-  const uuid = (await params).uuid;
-  try {
-    const attendance = await fetchCandidateDates(uuid);
-    return <RegisterPresentational attendance={attendance} />;
-  } catch {
+const RegisterContainer = ({ params }: RegisterContainerProps) => {
+  const uuid = use(params).uuid;
+  const { attendance, error, isLoading } = useAttendance(uuid);
+  if (isLoading) return <div className="bg-green-500">loading</div>; //TODO ローディングコンポーネント
+  if (error || !attendance) {
+    console.error(`データ取得に失敗： ${error?.message}` || 'No data returned.');
     redirect('/');
   }
+  return <RegisterPresentational attendance={attendance} />;
 };
 
 export default RegisterContainer;
--- src/app/attendance/[uuid]/register/page.tsx
+++ src/app/attendance/[uuid]/register/page.tsx
@@ -1,11 +1,5 @@
-import { Suspense } from 'react';
 import RegisterContainer from './RegisterContainer';
 
 export default function RegisterAttendance({ params }: { params: Promise<{ uuid: string }> }) {
-  return (
-    //TODO スケルトンUI
-    <Suspense fallback="loading">
-      <RegisterContainer params={params} />
-    </Suspense>
-  );
+  return <RegisterContainer params={params} />;
 }

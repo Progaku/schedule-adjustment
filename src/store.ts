b014028654f65fe6708c1330fc9d01fe01e7f3f7
masayasviel <33654883+masayasviel@users.noreply.github.com>
2025-04-12 23:28:29 +0900

    :sparkles: 予定内予定の取得

--- src/store.ts
+++ src/store.ts
@@ -7,3 +7,19 @@ export const attendanceAtom = atom<Attendance>({
   candidateDate: [],
   schedules: [],
 });
+
+export const participantAtom = atom<string | null>(null);
+
+export const getParticipantAtom = atom((get) => {
+  const participantUuid = get(participantAtom);
+  if (!participantUuid) {
+    return null;
+  }
+  let participant: Attendance['schedules'][number] | null = null;
+  get(attendanceAtom).schedules.forEach((item) => {
+    if (item.id === participantUuid) {
+      participant = item;
+    }
+  });
+  return participant;
+});

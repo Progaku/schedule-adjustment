c644635e438dcc7138b585a09ee1be8394de3232
masayasviel <33654883+masayasviel@users.noreply.github.com>
2025-04-12 23:11:26 +0900

    :sparkles: store依存に変更

--- src/app/attendance/[uuid]/register/RegisterContainer.tsx
+++ src/app/attendance/[uuid]/register/RegisterContainer.tsx
@@ -1,20 +1,15 @@
 'use client';
-import { useAttendance } from '@/hooks/useAttendance';
+
+import { attendanceAtom } from '@/store';
+import { useAtom } from 'jotai/index';
 import { redirect } from 'next/navigation';
-import { use } from 'react';
 import RegisterPresentational from './RegisterPresentational';
 
-type RegisterContainerProps = {
-  params: Promise<{ uuid: string }>;
-};
-
-const RegisterContainer = ({ params }: RegisterContainerProps) => {
-  const uuid = use(params).uuid;
-  const { attendance, error, isLoading } = useAttendance(uuid);
-  if (isLoading) return <div className="bg-green-500">loading</div>; //TODO ローディングコンポーネント
-  if (error || !attendance) {
-    console.error(`データ取得に失敗： ${error?.message}` || 'No data returned.');
-    redirect('/');
+const RegisterContainer = ({ uuid }: { uuid: string }) => {
+  const [attendance, _setAttendance] = useAtom(attendanceAtom);
+  if (!attendance || attendance.candidateDate.length === 0) {
+    redirect(`/attendance/${uuid}`);
+    return;
   }
   return <RegisterPresentational attendance={attendance} />;
 };

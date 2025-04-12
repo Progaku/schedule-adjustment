9c2002a2c58aae3893b7b05c27048337e19234ef
masayasviel <33654883+masayasviel@users.noreply.github.com>
2025-04-12 23:11:56 +0900

    :sparkles: 下地を整えた

--- src/app/attendance/[uuid]/page.tsx
+++ src/app/attendance/[uuid]/page.tsx
@@ -1,3 +1,8 @@
-export default function AttendanceDashboard() {
-  return <div>Attendance Dashboard</div>;
+import AttendanceContainer from '@/app/attendance/[uuid]/attendanceContainer';
+import { use } from 'react';
+
+export default function AttendanceDashboard({ params }: { params: Promise<{ uuid: string }> }) {
+  const uuid = use(params).uuid;
+
+  return <AttendanceContainer uuid={uuid} />;
 }

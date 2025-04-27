79d4912fa3db09cdbac15dfc366c225bf31dc6cc
masayasviel <33654883+masayasviel@users.noreply.github.com>
2025-04-27 17:27:17 +0900

    :sparkles:

--- src/app/attendance/[uuid]/register/RegisterContainer.tsx
+++ src/app/attendance/[uuid]/register/RegisterContainer.tsx
@@ -1,9 +1,9 @@
 'use client';
 
 import { attendanceAtom } from '@/store';
+import { SegmentedControl, SegmentedControlButton } from '@yamada-ui/react';
 import { useAtom } from 'jotai/index';
 import { redirect } from 'next/navigation';
-import RegisterPresentational from './RegisterPresentational';
 
 const RegisterContainer = ({ uuid }: { uuid: string }) => {
   const [attendance, _setAttendance] = useAtom(attendanceAtom);
@@ -11,7 +11,22 @@ const RegisterContainer = ({ uuid }: { uuid: string }) => {
     redirect(`/attendance/${uuid}`);
     return;
   }
-  return <RegisterPresentational attendance={attendance} />;
+  return (
+    <>
+      <div className="flex flex-col gap-3">
+        {attendance.candidateDate.map((date) => (
+          <div className="flex flex-row gap-2" key={date}>
+            <div>{date}</div>
+            <SegmentedControl variant="basic">
+              <SegmentedControlButton value="ok">○</SegmentedControlButton>
+              <SegmentedControlButton value="pn">△</SegmentedControlButton>
+              <SegmentedControlButton value="ng">×</SegmentedControlButton>
+            </SegmentedControl>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default RegisterContainer;
